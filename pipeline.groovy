39da682f058ff521aa23afe87f8aebb91dfd64af
suraj <surajbele51@gmail.com>
2024-12-26 09:27:42 +0530

    pipeline pull stage added

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('pull') { 
             steps {
-                echo  "code pulling successful"
+                git branch: 'main', url: 'https://github.com/SurajBele/studentapp.ui.git'
             }
         }
         stage('Build') { 
