d29ca386c1e231f65d6fd12ed42270d908da69de
suraj <surajbele51@gmail.com>
2025-01-06 23:00:30 +0530

    maven updated

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,14 +9,14 @@ pipeline {
         }
         stage('Build') { 
             steps {
-                sh '/opt/apache-maven-3.9.9/bin/mvn clean package'
+                sh 'mvn clean package'
                 echo "building successful" 
             }
         }
         stage('Test') { 
             steps {
                 withSonarQubeEnv(installationName: 'sonar-server', credentialsId: 'sonar-token') {
-                sh '/opt/apache-maven-3.9.9/bin/mvn sonar:sonar -Dsonar.projectKey=myproject'
+                sh 'mvn sonar:sonar -Dsonar.projectKey=myproject'
                 echo "testing is successful"
                 }
             }
