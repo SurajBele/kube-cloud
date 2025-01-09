9aafd8bdd7d0d90743e387c3dcb66bd36a59dc47
suraj <surajbele51@gmail.com>
2025-01-09 09:11:48 +0530

    terraform file added

---
+++ terrform/1st.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "myserver" {
+    ami = "ami-01816d07b1128cd2d"
+    key_name = "id_rsa" 
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [sg-0d34c3d2b6fa6492a]
+    availability_zone = "us-east-1a"
+    tags = {
+      name = "suraj"
+      env = "dev"
+    }
+}
