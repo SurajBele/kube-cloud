2923f805ae74889e445e7f56a83271f33a895c3d
suraj <surajbele51@gmail.com>
2025-01-11 09:07:31 +0530

    --- Auto Git Commit ---

--- terrform/variables.tf
+++ terrform/variables.tf
@@ -2,7 +2,7 @@ variable "vpc_cidr_block" {
    default = "10.0.0.0/20"
    description = "this is a vpc network"
 }
-variable "cidr" {
-   default = "10.0.0.0/20"
+variable "sg_cidr" {
+   default = "192.168.0.0/20"
    description = "this is a vpc network"
 }
