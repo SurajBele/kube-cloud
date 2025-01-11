6f1aaa73ff6c1c9f97ffc32553cc31c6df51fa0c
suraj <surajbele51@gmail.com>
2025-01-11 08:41:24 +0530

    --- Auto Git Commit ---

--- terrform/1st.tf
+++ terrform/1st.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 resource "aws_instance" "myserver" {
     ami = "ami-01816d07b1128cd2d"
-                         key_name = "id_rsa" 
+    key_name = "id_rsa" 
     instance_type = "t2.micro"
                                  vpc_security_group_ids = [ "sg-0d34c3d2b6fa6492a" ]
                  availability_zone = "us-east-1a"
