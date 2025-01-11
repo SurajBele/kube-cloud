dca55b5d809d5fbac9a3a1d7a587c0fbbab7e460
suraj <surajbele51@gmail.com>
2025-01-11 09:00:57 +0530

    --- Auto Git Commit ---

--- terrform/1st.tf
+++ terrform/1st.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "myserver" {
     ami = "ami-01816d07b1128cd2d"
     key_name = "id_rsa" 
     instance_type = "t2.micro"
-    vpc = aws_vpc.mynetwork
+    vpc = aws_vpc.mynetwork.id
     vpc_security_group_ids = aws_security_group.my_sg.id
     availability_zone = "us-east-1a"
     tags = {
