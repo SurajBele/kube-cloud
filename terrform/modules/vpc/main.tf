15e50e2c6e625a06677a4594633aaa29c6abb24b
suraj <surajbele51@gmail.com>
2025-01-14 08:54:24 +0530

    --- Auto Git Commit ---

--- terrform/modules/vpc/main.tf
+++ terrform/modules/vpc/main.tf
@@ -36,4 +36,8 @@ resource "aws_route_table" "myroute" {
       name = var.project
       env = var.env
     }
+    route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
 }
