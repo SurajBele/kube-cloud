4e677cc38cabbfa1169e76b9cfa0017ab17fe274
suraj <surajbele51@gmail.com>
2025-01-14 09:18:35 +0530

    --- Auto Git Commit ---

--- terrform/modules/instance/main.tf
+++ terrform/modules/instance/main.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "iron_man" {
     instance_type = var.instance_type
     vpc_security_group_ids = aws_security_group.firewall.id
     tags = {
-      name = "suraj"
-      env = "dev"
+      name = var.project
+      env = var.env
     }
     
 }
