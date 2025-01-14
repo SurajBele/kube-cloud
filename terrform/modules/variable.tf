c60112c58210bcd0ba7435f17a5af5871942c9bf
suraj <surajbele51@gmail.com>
2025-01-14 09:37:58 +0530

    --- Auto Git Commit ---

--- terrform/modules/variable.tf
+++ terrform/modules/variable.tf
@@ -17,8 +17,8 @@ variable "env" {
     default = "avengers"
 }
 variable "vpc_cidr" {
-    default = "10.0.0.0/20"
+    default = "10.0.0.0/16"
 }
 variable "pvt_subnet_cidr" {
-    default = 
+    default = "10.0.1.0/20"
 }
