863acf1c8c1638c9a27c85cb382df960e0d866ff
suraj <surajbele51@gmail.com>
2025-01-14 09:31:26 +0530

    --- Auto Git Commit ---

--- terrform/modules/variable.tf
+++ terrform/modules/variable.tf
@@ -2,7 +2,7 @@ variable "aws_region" {
   default = "us-east-1"
 }
 variable "ami_id" {
-    default = 
+    default = "ami-01816d07b1128cd2d"
 }
 variable "key_name" {}
 variable "instance_type" {}
