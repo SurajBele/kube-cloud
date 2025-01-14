23d16856c45704ffb453c5a7c673957c111ec0e2
suraj <surajbele51@gmail.com>
2025-01-14 08:51:08 +0530

    --- Auto Git Commit ---

--- terrform/modules/vpc/main.tf
+++ terrform/modules/vpc/main.tf
@@ -22,6 +22,6 @@ resource "aws_subnet" "pub_subnet" {
     }
     map_public_ip_on_launch = true
 }
-resource "aws_internet_gateway" "myigw" {
-  
+resource "aws_internet_gateway" "my_igw" {
+    
 }
