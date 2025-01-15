54d43c645e0a3e196c6dfbe78816ce227a9488db
suraj <surajbele51@gmail.com>
2025-01-15 15:43:23 +0530

    --- Auto Git Commit ---

--- terrform/modules/main.tf
+++ terrform/modules/main.tf
@@ -7,6 +7,38 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+resource "aws_security_group" "firewall" {
+  vpc_id = module.myvpc.vpc_id
+
+  # Allow HTTP traffic (port 80) from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow SSH traffic (port 22) from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    tags = {
+      name = var.project
+      env = var.env
+    }
+   
+}
 module "myvpc" {
   source = "./vpc"
   vpc_cidr = var.vpc_cidr
