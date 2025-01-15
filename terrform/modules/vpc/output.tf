60b746283c802da65b225af1202a20c5e9e104d8
suraj <surajbele51@gmail.com>
2025-01-15 09:22:53 +0530

    --- Auto Git Commit ---

--- terrform/modules/vpc/output.tf
+++ terrform/modules/vpc/output.tf
@@ -2,7 +2,7 @@ output "pvt_subnet_id" {
   value = aws_subnet.pvt_subnet.id
 }
 output "pub_subnet_id" {
-  value = aws_subnet.pub-subnet.id
+  value = aws_subnet.pub_subnet.id
 }
 output "vpc_id" {
   value = aws_vpc.myvpc.id
