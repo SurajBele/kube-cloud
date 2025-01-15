abc05a3f63f0f58f44a70413950eecfc4a645fc4
suraj <surajbele51@gmail.com>
2025-01-15 09:25:07 +0530

    --- Auto Git Commit ---

--- terrform/modules/vpc/main.tf
+++ terrform/modules/vpc/main.tf
@@ -46,30 +46,3 @@ resource "aws_route_table_association" "route_subnet" {
     route_table_id = aws_route_table.myroute.id
     
 }
-resource "aws_security_group" "firewall" {
-   vpc_id = aws_vpc.mynetwork.id
-    ingress = {
-       from_port = 80
-       to_port = 80
-       protocol = "TCP"
-       cidr = ["0.0.0.0/0"]
-    }
-    ingress {
-       from_port        = 22
-       to_port          = 22
-       protocol         = "TCP"
-       cidr_blocks      = ["0.0.0.0/0"]
-  }
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-
-   }
-    tags = {
-      name = var.project
-      env = var.env
-    }
-   
-}
