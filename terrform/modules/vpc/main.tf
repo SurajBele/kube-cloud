bca4dba47b9dafc2d84c10cf69679956c5b03c90
suraj <surajbele51@gmail.com>
2025-01-15 15:02:44 +0530

    --- Auto Git Commit ---

--- terrform/modules/vpc/main.tf
+++ terrform/modules/vpc/main.tf
@@ -50,29 +50,30 @@ resource "aws_route_table_association" "route_subnet" {
 }
 resource "aws_security_group" "firewall" {
   vpc_id = aws_vpc.mynetwork.id
-  ingress = [
-    {
-       from_port    = 80
-       to_port      = 80
-       protocol     = "TCP"
-       cidr_blocks  = ["0.0.0.0/0"]
-    },
-    {
-       from_port        = 22
-       to_port          = 22
-       protocol         = "TCP"
-       cidr_blocks      = ["0.0.0.0/0"]
-    } 
-]
-    
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
 
-   }
+  # Allow HTTP traffic (port 80) from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  # Allow SSH traffic (port 22) from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
     tags = {
       name = var.project
       env = var.env
