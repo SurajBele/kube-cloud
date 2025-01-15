5367f41425283a4c78dc4b54df86cf462a57d5dc
suraj <surajbele51@gmail.com>
2025-01-15 15:05:45 +0530

    --- Auto Git Commit ---

--- terrform/modules/variable.tf
+++ terrform/modules/variable.tf
@@ -20,7 +20,7 @@ variable "vpc_cidr" {
     default = "10.0.0.0/16"
 }
 variable "pvt_subnet_cidr" {
-    default = "10.0.1.0/20"
+    default = "10.0.0.0/20"
 }
 variable "pub_subnet_cidr" {
     default = "10.0.2.0/20"
