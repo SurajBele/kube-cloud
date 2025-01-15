25b9c9298592416cbb04ba65c438bce8b58af6e8
suraj <surajbele51@gmail.com>
2025-01-15 09:43:48 +0530

    --- Auto Git Commit ---

--- terrform/modules/main.tf
+++ terrform/modules/main.tf
@@ -20,7 +20,7 @@ module "aws_instance" {
   ami_id = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  subnet_id = module.myvpc.pub_subnet_id
+  subnet_id = module.myvpc.pub_subnet.id
   project = var.project
   env = var.env
 }
