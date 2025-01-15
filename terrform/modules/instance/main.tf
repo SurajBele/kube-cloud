6c935e5a9cafe20a5b6401c6f8574c87fe064331
suraj <surajbele51@gmail.com>
2025-01-15 15:22:06 +0530

    --- Auto Git Commit ---

--- terrform/modules/instance/main.tf
+++ terrform/modules/instance/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "iron_man" {
     ami = var.ami_id
     key_name = var.key_name
     instance_type = var.instance_type
-    vpc_security_group_ids = [ "aws_security_group.firewall.id" ]
+    vpc_security_group_ids = [ aws_security_group.firewall.id ]
     tags = {
       name = var.project
       env = var.env
