8901d167e537d493aee34ca56a2da9b79094ba24
suraj <surajbele51@gmail.com>
2025-01-20 08:48:25 +0530

    --- Auto Git Commit ---

--- terrform/main.tf
+++ terrform/main.tf
@@ -9,18 +9,18 @@
 #   region = "us-east-1"
   
 # }
-# resource "aws_instance" "myserver" {
-#     ami = "ami-01816d07b1128cd2d"
-#     key_name = "id_rsa" 
-#     instance_type = "t2.micro"
-#     vpc_security_group_ids = aws_security_group.my_sg.id
-#     availability_zone = "us-east-1a"
-#     tags = {
-#       name = "suraj"
-#       env = "dev"
-#     }
+resource "aws_instance" "myserver" {
+    ami = "ami-01816d07b1128cd2d"
+    key_name = "id_rsa" 
+    instance_type = "t2.micro"
+    vpc_security_group_ids = aws_security_group.my_sg.id
+    availability_zone = "us-east-1a"
+    tags = {
+      name = "suraj"
+      env = "dev"
+    }
     
-# }
+}
 
 # resource "aws_security_group" "my_sg" {
 #     name = "my_sg"
